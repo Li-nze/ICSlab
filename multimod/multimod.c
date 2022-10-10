8a5c5c4a355e110c523c8f7d9a06ab8f8ae3e09f
211300034-He Linzhe <211300034@smail.nju.edu.cn>
2022-10-10 22:46:18 +0800

    second lab1 keso!

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -6,11 +6,9 @@
 uint64_t umd=0xffffffffffffffff;
 static uint64_t md(uint64_t a, uint64_t m){
 	while(a<0){a+=m;}
-	//printf("%lu\n%lu\n", a, m);
 	while(a>=m){
 		a-=m;
 	}
-	//printf("%lu\n%lu\n", a, m);
 	return a;
 }
 	
@@ -33,14 +31,11 @@ static uint64_t addmd(uint64_t a, uint64_t b, uint64_t m){
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m){
 	umd=md(umd, m);
 	a=md(a, m);
-	//printf("a: %lu\n", a);
 	b=md(b, m);
-	printf("%lu %lu %lu\n", a, b, m);
 	uint64_t c=0;
 	uint64_t tmp=b;
 	for(int i=0; i<64; ++i){
 		if(bit_of(a, i)==1){
-			//printf("i:%d\n", i);
 			c=addmd(c, tmp, m);
 		}
 		tmp=addmd(tmp, tmp, m);
